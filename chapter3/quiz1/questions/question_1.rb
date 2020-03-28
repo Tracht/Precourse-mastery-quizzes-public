14655d31f8ddc398b75ac074d91d9651165df9a2
tracht <marija.trachtenberg@gmail.com>
2020-03-28 16:43:01 +0000

    Quiz 3

--- chapter3/quiz1/questions/question_1.rb
+++ chapter3/quiz1/questions/question_1.rb
@@ -1,18 +1,19 @@
+#!/Users/marijatrachtenberg/.rvm/rubies/ruby-2.6.5/bin/ruby
 # Uncomment each line and fill in the blanks so that the arithmetic
 # operations produce the correct value for each variable.  e.g. in
 # `four = 2.+()`, the calculation to the right of the `=` should
 # evaluate to 4. So it would become `four = 2.+(2)`.
 
-# five = 2.+()
+five = 2.+(3)
 
-# seven = 14./()
+seven = 14./(2)
 
-# two = 2.*()./()
+two = 2.*(2)./(2)
 
-# three_point_zero = 6.to_f./()
+three_point_zero = 6.to_f./(2)
 
-# two_is_integer = 4.to_f./().to_i.integer?
+two_is_integer = 4.to_f./(2).to_i.integer?
 
-# four = 12.%()
+four = 12.%(8)
 
-# zero = 12.%()
+zero = 12.%(6)
