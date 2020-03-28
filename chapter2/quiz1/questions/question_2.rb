5ea19c9c2f57748a6f7d0cc93ccca0eb1dcbba45
tracht <marija.trachtenberg@gmail.com>
2020-03-28 13:20:22 +0000

    Quiz 2: questions 1 and 2

--- chapter2/quiz1/questions/question_2.rb
+++ chapter2/quiz1/questions/question_2.rb
@@ -1,2 +1,7 @@
+#!/Users/marijatrachtenberg/.rvm/rubies/ruby-2.6.5/bin/ruby
 # Write a program that multiplies `TWELVE` by `FOURTEEN` by `SIXTEEN`
 # and `puts`es the result.
+TWELVE = 12
+FOURTEEN = 14
+SIXTEEN = 16
+puts TWELVE  * FOURTEEN * SIXTEEN 
